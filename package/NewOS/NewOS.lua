2f698318de09b425ac5f5c577ca3b559de933494
kiz___ <140178579+kizYTB@users.noreply.github.com>
2024-11-19 19:31:48 +0100

    Create NewOS.lua

---
+++ package/NewOS/NewOS.lua
@@ -0,0 +1,10 @@
+return {
+  name = "NewOS",
+  version = "1.0.2",
+  description = "un os computercraft",
+  dependencies = {},
+  install = {
+    type = "wget",
+    code = "https://kiz-data.jtheberg.fr/CCMC/OS/install/install.lua"
+  }
+}
