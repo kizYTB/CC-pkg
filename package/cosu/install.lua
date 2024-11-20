41a082dbc4f223cbab1be9b60a6c17620e2b3405
kiz___ <140178579+kizYTB@users.noreply.github.com>
2024-11-20 09:54:56 +0100

    update rm vericode add CONSULT

---
+++ package/cosu/install.lua
@@ -0,0 +1,2 @@
+shell.run("cd","/")
+shell.run("wget","https://github.com/1Turtle/consult/releases/latest/download/cosu.lua")
--- package/vericode/install.lua
+++
@@ -1,5 +0,0 @@
-local run = shell.run
-
-run("cd","/")
-
-run("wget","https://raw.githubusercontent.com/kizYTB/CC-pkg/refs/heads/main/package/vericode/vericode.lua")
